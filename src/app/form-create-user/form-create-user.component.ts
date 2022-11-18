7a7bcc4f99a34162853132334c8f24f6e9087b81
benoitbelloc <benoit.belloc@ynov.com>
2022-11-18 17:10:31 +0100

    paiement ^^

--- src/app/form-create-user/form-create-user.component.ts
+++ src/app/form-create-user/form-create-user.component.ts
@@ -11,6 +11,4 @@ export class FormCreateUserComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  
 }
--- src/app/pop-up-payment/pop-up-payment.component.ts
+++ src/app/pop-up-payment/pop-up-payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IPayPalConfig, ICreateOrderRequest } from 'ngx-paypal';
 
 @Component({
   selector: 'app-pop-up-payment',
@@ -6,10 +7,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pop-up-payment.component.scss']
 })
 export class PopUpPaymentComponent implements OnInit {
+  
+  showSuccess: boolean = false;
 
-  constructor() { }
+  public payPalConfig?: IPayPalConfig;
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      this.initConfig();
+    }
+
+    private initConfig(): void {
+      this.payPalConfig = {
+      currency: 'EUR',
+      clientId: 'sb',
+      createOrderOnClient: (data) => <ICreateOrderRequest>{
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'EUR',
+              value: '9.99',
+              breakdown: {
+                item_total: {
+                  currency_code: 'EUR',
+                  value: '9.99'
+                }
+              }
+            },
+            items: [
+              {
+                name: 'Enterprise Subscription',
+                quantity: '1',
+                category: 'DIGITAL_GOODS',
+                unit_amount: {
+                  currency_code: 'EUR',
+                  value: '9.99',
+                },
+              }
+            ]
+          }
+        ]
+      },
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        label: 'paypal',
+        layout: 'vertical'
+      },
+      onApprove: (data, actions) => {
+        console.log('onApprove - transaction was approved, but not authorized', data, actions);
+        actions.order.get().then((details: any) => {
+          console.log('onApprove - you can get full order details inside onApprove: ', details);
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+        this.showSuccess = true;
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+      },
+      onError: err => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      },
+    };
   }
-
 }
