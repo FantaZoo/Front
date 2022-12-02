468a0cb5bda20b94ae32857f1be8b2cd110bd439
laura398 <lauramathieu.git@gmail.com>
2022-12-02 09:52:49 +0100

    getting data from db for descritpion and admin board catalogue

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Diet } from '../share/diet';
 import { Species } from '../share/species';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-description',
@@ -8,31 +11,44 @@ import { Species } from '../share/species';
   styleUrls: ['./description.component.scss']
 })
 export class DescriptionComponent implements OnInit {
-  animal = {
-    id: 1,
-    animal_name: 'Phoenix',
-    image: 'https://imagizer.imageshack.com/a/img923/4374/IGrnJh.png',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
-    price: 1000,
-    status: "Disponible",
-    species: Species.Phoenix, 
-    sexe: 'Femelle',
-    age: 2,
-    diet: Diet.Omnivore
-  };
+  href: string = this.router.url.split('/')[2];
+  
+  // animal = {
+  //   id: 1,
+  //   animal_name: 'Phoenix',
+  //   image: 'https://imagizer.imageshack.com/a/img923/4374/IGrnJh.png',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
+  //   price: 1000,
+  //   status: "Disponible",
+  //   species: Species.Phoenix, 
+  //   sexe: 'Femelle',
+  //   age: 2,
+  //   diet: Diet.Omnivore
+  // };
 
-  dataToDisplay = [
-    {name: 'Age', value: this.animal.age},
-    {name: 'Sexe', value: this.animal.sexe},
-    {name: 'Régime', value: this.animal.diet},
-    {name: 'Statut', value: this.animal.status},
-    {name: "Prix", value: this.animal.price}
-  ]
+  animal: any;
 
-  constructor() { }
+  // dataToDisplay = [
+  //   {name: 'Age', value: this.animal.age},
+  //   {name: 'Sexe', value: this.animal.sexe},
+  //   {name: 'Régime', value: this.animal.diet},
+  //   {name: 'Statut', value: this.animal.status},
+  //   {name: "Prix", value: this.animal.price}
+  // ]
+
+  sexe: string = "";
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
-    console.log(this.animal)
+    console.log("url : ", `${environment.url}/animals/${this.href}/`);
+    
+    this.http.get(`${environment.url}/animals/${this.href}/`)
+      .subscribe((data) => {
+        console.log(data);
+        this.animal = data;
+        this.sexe = this.animal.sexe === 'M' ? 'Mâle' : 'Femelle';
+      });
     
   }
 
